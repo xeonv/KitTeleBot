297114ca7c452175ae57dfd544910cfdecc3ff26
Valerii <xeonv@mail.ru>
2024-10-08 22:24:00 +0300

    add command settimer

--- bot.py
+++ bot.py
@@ -63,6 +63,33 @@ async def cmd_test1(message: types.Message, test1, test2):
     await message.answer(f'Выводим test1: {test1} test2: {test2}')
 
 
+@dp.message(Command("settimer"))
+async def cmd_settimer(
+        message: types.Message,
+        command: CommandObject
+):
+    # Если не переданы никакие аргументы, то
+    # command.args будет None
+    if command.args is None:
+        await message.answer(
+            "Ошибка: не переданы аргументы"
+        )
+        return
+    # Пробуем разделить аргументы на две части по первому встречному пробелу
+    try:
+        delay_time, text_to_send = command.args.split(" ", maxsplit=1)
+    # Если получилось меньше двух частей, вылетит ValueError
+    except ValueError:
+        await message.answer(
+            "Ошибка: неправильный формат команды. Пример:\n"
+            "/settimer time message"
+        )
+        return
+    await message.answer(
+        "Таймер добавлен!\n"
+        f"Время: {delay_time}\n"
+        f"Текст: {text_to_send}"
+    )
 
 
 @dp.message(Command("info"))
