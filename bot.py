4c5e957d2b9bf98ed8985c4d781f7d328c84316e
Valerii <xeonv@mail.ru>
2024-10-29 18:56:54 +0300

    test special buttons

--- bot.py
+++ bot.py
@@ -138,6 +138,46 @@ async def reply_builder(message: types.Message):
         reply_markup=builder.as_markup(resize_keyboard=True, one_time_keyboard=True),
     )
 
+@dp.message(Command("special_buttons"))
+async def cmd_special_buttons(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    # метод row позволяет явным образом сформировать ряд
+    # из одной или нескольких кнопок. Например, первый ряд
+    # будет состоять из двух кнопок...
+    builder.row(
+        types.KeyboardButton(text="Запросить геолокацию", request_location=True),
+        types.KeyboardButton(text="Запросить контакт", request_contact=True)
+    )
+    # ... второй из одной ...
+    builder.row(types.KeyboardButton(
+        text="Создать викторину",
+        request_poll=types.KeyboardButtonPollType(type="quiz"))
+    )
+    # ... а третий снова из двух
+    builder.row(
+        types.KeyboardButton(
+            text="Выбрать премиум пользователя",
+            request_user=types.KeyboardButtonRequestUser(
+                request_id=1,
+                user_is_premium=True
+            )
+        ),
+        types.KeyboardButton(
+            text="Выбрать супергруппу с форумами",
+            request_chat=types.KeyboardButtonRequestChat(
+                request_id=2,
+                chat_is_channel=False,
+                chat_is_forum=True
+            )
+        )
+    )
+    # WebApp-ов пока нет, сорри :(
+
+    await message.answer(
+        "Выберите действие:",
+        reply_markup=builder.as_markup(resize_keyboard=True),
+    )
+
 @dp.message(Command('images'))
 async def upload_photo(message: Message):
     # Сюда будем помещать file_id отправленных файлов, чтобы потом ими воспользоваться
