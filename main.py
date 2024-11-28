3438a6cd39775eef385695ff9305d3232585e2f5
xeonv <xeonv@mail.ru>
2024-11-28 18:56:01 +0300

    Connecting fb db

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ from datetime import datetime
 from config_reader import config
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
+from firebird.driver import connect, driver_config, DESCRIPTION_NAME, DESCRIPTION_DISPLAY_SIZE
 
+# Connecting to IT Okna DB
+driver_config.read('myapp.cfg')
+con = connect('employee')
 
 # Enable logging so as not to miss important messages
 logging.basicConfig(level=logging.INFO)
@@ -26,7 +30,7 @@ dp = Dispatcher()
 dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
 
 
-# Handler for the command /start + deep_link
+# Handler for the command /start + deep_link with id_doc '?start=iddoc_xxxxx'
 pattern_iddoc = re.compile(r'iddoc_(\d+)')
 
 @dp.message(CommandStart(
@@ -37,9 +41,14 @@ async def cmd_start_dl(
    message: types.Message,
    command: CommandObject
 ):
-    id_doc = command.args.split("_")[1]
-    await message.answer(f'ID_DOC {id_doc}')
-
+    try:
+        cur = con.cursor()
+        id_doc = command.args.split("_")[1]
+        cur.execute(f'select fin_vid from doc_acc_zag where id_doc = {id_doc}')
+        answer_from_db = str(cur.fetchone()[0])
+        await message.answer(f'Ответ: {answer_from_db}')
+    except TypeError:
+        await message.answer("По этому заказу нет информации")
 
 # Handler for the command /start
 @dp.message(Command("start"))
@@ -57,7 +66,7 @@ async def cmd_start_help(message: Message):
 
 # Handler for any message
 @dp.message()
-async def any_message(message: Message):
+async def answer_any_message(message: Message):
     await message.answer("Я непонимать вас мой господин!")
 
 
